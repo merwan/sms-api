c48f3596b8568d95defa299b23246dc9b131e927
Merouane Atig <merouane@teezily.com>
2017-07-27 19:06:45 +0200

    Setup sequelize

--- libs/boot.js
+++ libs/boot.js
@@ -1,6 +1,8 @@
 module.exports = app => {
-  const PORT = app.get("port");
-  app.listen(PORT, () => {
-    console.log(`SMS API - Port ${PORT}`)
+  app.db.sync().done(() => {
+    const PORT = app.get("port");
+    app.listen(PORT, () => {
+      console.log(`SMS API - Port ${PORT}`)
+    });
   });
 };
---
+++ libs/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  database: "sms",
+  username: "",
+  password: "",
+  params: {
+    host: "db",
+    dialect: "postgres",
+    define: {
+      underscored: true
+    }
+  }
+};
