58a2653ddb2a667bd22bbbbc963f3f4fc83218c3
Merouane Atig <merwan@patem.net>
2017-09-28 23:11:16 +0200

    Synchronize db during boot, even for tests

--- libs/boot.js
+++ libs/boot.js
@@ -1,10 +1,10 @@
 module.exports = app => {
-  if (process.env.NODE_ENV !== "test") {
-    app.db.sequelize.sync().done(() => {
+  app.db.sequelize.sync().then(() => {
+    if (process.env.NODE_ENV !== "test") {
       const PORT = app.get("port");
       app.listen(PORT, () => {
         console.log(`SMS API - Port ${PORT}`)
       });
-    });
-  }
+    }
+  });
 };
